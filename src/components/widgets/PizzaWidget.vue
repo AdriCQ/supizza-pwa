a312bb34448a068b50181468796cf7f12c8ae648
AdriCQ <adrian.capote95@zoho.com>
2023-02-16 22:51:54 -0500

    add title to widgets

--- src/components/widgets/PizzaWidget.vue
+++ src/components/widgets/PizzaWidget.vue
@@ -5,12 +5,17 @@ defineProps<{ pizza: IPizza }>();
 </script>
 
 <template>
-  <div class="w-full bg-white">
+  <div class="relative w-full rounded-md bg-white">
     <img
       :src="pizza.img"
       :alt="pizza.title"
       :title="pizza.title"
       class="w-full"
     />
+    <div
+      class="absolute bottom-1 left-1 w-full rounded-full bg-slate-100 p-2 text-center text-sm opacity-90"
+    >
+      {{ pizza.title }}
+    </div>
   </div>
 </template>
--- src/components/widgets/PromoWidget.vue
+++ src/components/widgets/PromoWidget.vue
@@ -5,12 +5,18 @@ defineProps<{ promo: IPromo }>();
 </script>
 
 <template>
-  <div class="w-full bg-white">
+  <div class="relative w-full bg-white">
     <img
       :src="promo.img"
       :alt="promo.title"
       :title="promo.title"
       class="w-full"
     />
+
+    <div
+      class="absolute bottom-1 left-1 w-full rounded-full bg-slate-100 p-2 text-center text-sm opacity-90"
+    >
+      {{ promo.title }}
+    </div>
   </div>
 </template>
