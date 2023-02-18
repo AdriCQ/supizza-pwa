fad52a4396d97380d7ff4d6726a9d052a66eb13e
AdriCQ <adrian.capote95@zoho.com>
2023-02-18 16:54:27 -0500

    android support

---
+++ android/app/src/main/java/mx/supizza/app/MainActivity.java
@@ -0,0 +1,5 @@
+package mx.supizza.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "mx.supizza.app",
+  appName: "supizza-pwa",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;
